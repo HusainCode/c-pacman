f589114b8ea3e4b16de08cfb10ea0c9fcefc695f
HusainCode <alshaikhahmed@hotmail.com>
2025-07-19 19:44:00 -0500

    Started Pac-Man

---
+++ src/game.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "../include/ghost.h"
+
+
+
+int main() {
+    printf("Welcome to Pac-Man!\n");
+    return 0;
+}
---
+++ src/pacman.c
@@ -0,0 +1,26 @@
+#include "../include/pacman.h"
+#include <stdio.h>
+
+
+// Initialize Pac-Man at givin position
+Pacman pacman_init(int x, int y){
+    Pacman p = {0};
+    p.x = x;
+    p.y = y;
+    p.lives = 3; // 3 lives just like the real game
+    p.direction = 0;
+
+    return p;
+
+}
+
+void pacmanMove(Pacman *p, int dx, int dy){
+    if(!p) return;
+    p->x += dx;
+    p->y += dy;
+}
+
+void pacmanDraw(const Pacman *p){
+    if(!p) return;
+    printf("PacMan is at(%d, %d)\n", p->x, p->y);
+}
