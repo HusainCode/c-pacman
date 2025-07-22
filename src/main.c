0b792bb37a99215b3320bc59f6378d22a9b86a4a
HusainCode <alshaikhahmed@hotmail.com>
2025-07-21 20:42:12 -0500

    Fixing ghost, pacman

--- src/main.c
+++ src/main.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
+#include "../include/gui.h"
 
 int main() {
+
     printf("Welcome to Pac-Man!\n");
+
+    gui_init();
+
+    SDL_Event event;
+    int running = 1;
+
+    // the game loop runining, till press X 
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = 0;
+            }
+        }
+
+        SDL_Delay(10); // Keep it responsive
+    }
+
+    gui_quit();
     return 0;
 }
---
+++ test/test.c
@@ -0,0 +1,14 @@
+#include "unity.h"
+
+
+void test_score(void){
+
+}
+
+int main(void){
+    UNITY_BEGIN();
+
+    // RUN_TEST();
+    return UNITY_END();
+
+}
