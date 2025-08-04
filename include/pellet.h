524bb30dea0708f07c84fac65350cf70f4b8d7f0
HusainCode <alshaikhahmed@hotmail.com>
2025-08-03 20:02:33 -0500

    fixing methods

--- include/pellet.h
+++ include/pellet.h
@@ -8,12 +8,22 @@
 #define TITLE_PELLET 2 // normal pellet
 #define TITLE_POWER_PELLET 3 // power-up pellet
 
+#define MAX_REGULAR_PELLETS 240
+#define MAX_POWER_PELLETS 4
+
 
 // Global pellte state
 extern int map[MAP_HEIGHT][MAP_WIDTH]; // Game tilemap
 extern int score;                      // Player score
 extern int pelltesRemaining;          // Remaining pellets on the map
 extern int powerPelltesRemaining;
+
+
+typedef struct pellet
+{
+    int x, y;
+} Pellet;
+
 // Pellet operations 
 // Pellet lifecycle
 void spawnPellet(int x, int y);
--- src/pellet.c
+++ src/pellet.c
@@ -18,6 +18,35 @@ void spawnPowerPellet(int x, int y){
         powerPelltesRemaining++;
     }
 }
-int isPelletAt(int x, int y);
 
-void collect_pellet(int x, int y);
+// Check if pellet is power or reguluar
+int isPelletAt(int x, int y) {
+    return (gameMap[y][x] == TITLE_PELLET || TITLE_POWER_PELLET);
+
+}
+
+
+// Collects a pellet at the given tile and updates scores/state 
+void collect_pellet(int x, int y){
+    // || gameMap[y][x] == TITLE_POWER_PELLET
+    if(gameMap[y][x] == TITLE_PELLET ){
+        gameMap[y][x] == TITLE_EMPTY;
+        gameScore += 10; // regular pellet
+        pelltesRemaining --;
+    }
+    else if (gameMap[y][x] == TITLE_POWER_PELLET){
+        gameMap[y][x] == TITLE_EMPTY;
+        gameScore +=50;
+        powerPelltesRemaining--;
+         // TODO: trigger power-up state for ghosts
+    }
+
+}
+
+void resetPellets(void){
+    if(powerPelltesRemaining == 0 && pelltesRemaining == 0){
+        for(int y = 0; y < MAP_HEIGHT; y++){
+          // STOPED HERE
+        }
+    }
+}
